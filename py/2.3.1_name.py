91f47a20d65495b1f57788bcbe2020deb00d03d9
Zhongdong Wang <44559384+Rtx8080Ti@users.noreply.github.com>
2019-12-26 21:44:09 +0800

    Update 2.3.1_name.py

--- py/2.3.1_name.py
+++ py/2.3.1_name.py
@@ -1,20 +1,22 @@
 name = "ada lovelace"
 print(name.title())
+print("over")
 
 name = "Ada Lovelace"
 print(name.upper())
 print(name.lower())
-print("结束")
+print("over")
 
 first_name = "ada"
 last_name = "lovelace"
 full_name = first_name + " " + last_name
 
 print (full_name)
-print("结束")
+print("over")
 
 print("Hello," + full_name.title() + "！")
-print("jieshu")
+print("over")
 
 message = "Hello," + full_name.title() + "!"
 print(message)
+print("over")
