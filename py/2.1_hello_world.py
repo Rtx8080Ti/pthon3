63c0e7d37ccdb26f773ae4b96531b6e10644bf67
Rtx8080Ti <384080039@qq.com>
2019-12-26 21:29:44 +0800

    test

---
+++ py/2.1_hello_world.py
@@ -0,0 +1,6 @@
+message = 'python可以用"这种方法"'
+print(message)
+
+message2 = "消息2"
+print(message2)
+
---
+++ py/2.3.1_name.py
@@ -0,0 +1,20 @@
+name = "ada lovelace"
+print(name.title())
+
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+print("结束")
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+
+print (full_name)
+print("结束")
+
+print("Hello," + full_name.title() + "！")
+print("jieshu")
+
+message = "Hello," + full_name.title() + "!"
+print(message)
